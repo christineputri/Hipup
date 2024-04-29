74b59d2a60919d62e8a0da25609a1c1ae8d0caed
christineputri <christineputri21@gmail.com>
2024-04-29 21:56:11 +0700

    Initial Commit

---
+++ Hipup Watch App/HipupApp.swift
@@ -0,0 +1,17 @@
+//
+//  HipupApp.swift
+//  Hipup Watch App
+//
+//  Created by Christine Putri on 29/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Hipup_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
