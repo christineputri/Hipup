bcbe5951868008a22766653896dac369236ef778
christineputri <christineputri21@gmail.com>
2024-05-01 19:06:04 +0700

    lanjut tidur bos

--- Hipup Watch App/FirstView.swift
+++ Hipup Watch App/FirstView.swift
@@ -1,9 +1,64 @@
+////
+////  First.swift
+////  Hipup Watch App
+////
+////  Created by Christine Putri on 29/04/24.
+////
 //
-//  First.swift
-//  Hipup Watch App
+//import SwiftUI
 //
-//  Created by Christine Putri on 29/04/24.
+//struct FirstView: View {
+//    @State private var isSecondViewPresented = false
+//    
+//    var body: some View {
+//        NavigationView {
+//            ZStack {
+//                ImageSequenceView1(imageNames1: getItem1())
+//                    .edgesIgnoringSafeArea(.all)
+//                
+//                NavigationLink(destination: SecondView().navigationBarBackButtonHidden(true), isActive: $isSecondViewPresented) {
+//                    EmptyView()
+//                }
+//            }
+//            .onTapGesture {
+//                self.isSecondViewPresented = true
+//            }
+//        }
+//    }
+//    
+//    func getItem1() -> [String] {
+//        var imagesPath: [String] = []
+//        for i in 0..<89 {
+//            imagesPath.append("Gestrue_Tangan_Fix_\(String(format: "%05d", i))")
+//        }
+//        return imagesPath
+//    }
+//}
 //
+//struct ImageSequenceView1: View {
+//    var imageNames1: [String]
+//    @State private var currentIndex1 = 0
+//    
+//    var body: some View {
+//        Image(imageNames1[currentIndex1])
+//            .resizable()
+//            .scaledToFill()
+//            .edgesIgnoringSafeArea(.all)
+//            .onAppear {
+//                Timer.scheduledTimer(withTimeInterval: 0.033, repeats: true) { timer in
+//                    currentIndex1 = (currentIndex1 + 1) % imageNames1.count
+//                }
+//            }
+//    }
+//}
+//
+//struct FirstView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        FirstView()
+//    }
+//}
+
+
 
 import SwiftUI
 
@@ -15,13 +70,13 @@ struct FirstView: View {
             ZStack {
                 ImageSequenceView1(imageNames1: getItem1())
                     .edgesIgnoringSafeArea(.all)
-                
-                NavigationLink(destination: SecondView().navigationBarBackButtonHidden(true), isActive: $isSecondViewPresented) {
-                    EmptyView()
-                }
             }
+            .navigationBarHidden(true) // Hide the navigation bar
             .onTapGesture {
                 self.isSecondViewPresented = true
+            }
+            .sheet(isPresented: $isSecondViewPresented) {
+                SecondView()
             }
         }
     }
--- Hipup Watch App/ThirdView.swift
+++ Hipup Watch App/ThirdView.swift
@@ -1,15 +1,96 @@
+////
+////  ThirdView.swift
+////  Hipup Watch App
+////
+////  Created by Christine Putri on 29/04/24.
 //
-//  ThirdView.swift
-//  Hipup Watch App
+//import SwiftUI
+//import AVFoundation
 //
-//  Created by Christine Putri on 29/04/24.
+//struct ThirdView: View {
+//    @State private var isFirstViewPresented = false
+//    @Binding var timer:Timer?
+//    
+//    let audioPath = AVPlayer(url: Bundle.main.url(forResource: "flush", withExtension: "wav")!)
+//    @Environment(\.presentationMode) var presentationMode
+//    
+//    @StateObject var audioPlayer = AudioPlayer()
+//   
+//    var body: some View {
+//        ZStack {
+//            ImageSequenceView3(imageNames3: getItem3())
+//                .edgesIgnoringSafeArea(.all)
+//                
+//            NavigationLink(destination: FirstView().navigationBarBackButtonHidden(true), isActive: $isFirstViewPresented) {
+//                EmptyView()
+//            }
+//                .onTapGesture {
+//                    self.isFirstViewPresented = true
+//                }
+////                .fullScreenCover(isPresented: $isThirdViewPresented, content: {
+////                    ThirdView()
+////                })
+//        }
+//        .task {
+//            Task {
+//                timer?.invalidate()
+//            }
+//            audioPath.play()
+//        }
+//        .navigationBarBackButtonHidden(true)
+//        .ignoresSafeArea()
+////        .onAppear {
+////            DispatchQueue.main.asyncAfter(deadline: .now()) {
+////                self.audioPath.play()
+////                Timer.scheduledTimer(withTimeInterval: 0.5 * Double(["mao"].count), repeats: false) { timer in
+////                    self.audioPath.volume = 0
+////                }
+////            }
+////        }
+//    }
+//}
+//
+//class AudioPlayer: ObservableObject {
+//    var audioPlayer: AVAudioPlayer?
+//}
+//
+//func getItem3() -> [String] {
+//    var imagesPath: [String] = []
+//    for i in 0..<89 {
+//        imagesPath.append("Toilet fix for AE_\(String(format: "%05d", i))")
+//    }
+//    return imagesPath
+//}
+//
+//struct ImageSequenceView3: View {
+//    var imageNames3: [String]
+//    @State private var currentIndex3 = 0
+//    
+//    var body: some View {
+//        Image(imageNames3[currentIndex3])
+//            .resizable()
+//            .scaledToFill()
+//            .edgesIgnoringSafeArea(.all)
+//            .onAppear {
+//                Timer.scheduledTimer(withTimeInterval: 0.03, repeats: true) { timer in
+//                    currentIndex3 = (currentIndex3 + 1) % imageNames3.count
+//                }
+//            }
+//    }
+//}
+//
+//struct ThirdView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ThirdView(timer: .constant(Timer()))
+//    }
+//}
 
 import SwiftUI
 import AVFoundation
 
 struct ThirdView: View {
     @State private var isFirstViewPresented = false
-    @Binding var timer:Timer?
+    @Binding var timer: Timer?
     
     let audioPath = AVPlayer(url: Bundle.main.url(forResource: "flush", withExtension: "wav")!)
     @Environment(\.presentationMode) var presentationMode
@@ -24,12 +105,10 @@ struct ThirdView: View {
             NavigationLink(destination: FirstView().navigationBarBackButtonHidden(true), isActive: $isFirstViewPresented) {
                 EmptyView()
             }
-                .onTapGesture {
-                    self.isFirstViewPresented = true
-                }
-//                .fullScreenCover(isPresented: $isThirdViewPresented, content: {
-//                    ThirdView()
-//                })
+            .buttonStyle(PlainButtonStyle()) // Hide the navigation link button
+            .onTapGesture {
+                self.isFirstViewPresented = true
+            }
         }
         .task {
             Task {
@@ -39,14 +118,6 @@ struct ThirdView: View {
         }
         .navigationBarBackButtonHidden(true)
         .ignoresSafeArea()
-//        .onAppear {
-//            DispatchQueue.main.asyncAfter(deadline: .now()) {
-//                self.audioPath.play()
-//                Timer.scheduledTimer(withTimeInterval: 0.5 * Double(["mao"].count), repeats: false) { timer in
-//                    self.audioPath.volume = 0
-//                }
-//            }
-//        }
     }
 }
 
